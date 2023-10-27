c059c87b2cb864a3cf62765a652aba5ad694efe4
Freddelan <happyrush@hotmail.com>
2023-10-27 12:07:34 +0200

    Morning commit13

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-
+import { HashRouter as Router } from "react-router-dom";
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 reportWebVitals();
