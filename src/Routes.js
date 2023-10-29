e637f2ccaef22eab93c691fb63e3a5ecd1c9245c
Freddelan <happyrush@hotmail.com>
2023-10-29 21:20:41 +0100

    commit du soir bonsoir2

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import About from "./pages/apropos";
 import RGPD from "./pages/RGPD";
