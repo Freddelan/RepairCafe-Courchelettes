e29abf2d4335137caa223e96e7e373cf0d6dcf5f
Freddelan <happyrush@hotmail.com>
2023-10-30 18:47:53 +0100

    Updates

--- Routes.js
+++ Routes.js
@@ -1,5 +1,6 @@
+// LaRoute.js
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import About from "./pages/apropos";
 import RGPD from "./pages/RGPD";
@@ -7,16 +8,14 @@ import Membres from "./pages/membres";
 
 function LaRoute() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/apropos" element={<About />} />
-          <Route path="/rgpd" element={<RGPD />} />
-          <Route path="/membres" element={<Membres />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/apropos" element={<About />} />
+        <Route path="/rgpd" element={<RGPD />} />
+        <Route path="/membres" element={<Membres />} />
+      </Routes>
+    </Router>
   );
 }
 
