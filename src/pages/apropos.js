5749f2107b6461c751e074b48a995282e87fa65e
Freddelan <happyrush@hotmail.com>
2023-10-30 21:04:49 +0100

    gh-pages

--- src/pages/apropos.js
+++ src/pages/apropos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../scss/apropos.scss";
-import Navigation from "../components/Navigation";
-import Footer from "../components/Footer";
+import Navigation from "../../components/Navigation";
+import Footer from "../../components/Footer";
 
 const Apropos = () => {
   return (
